cba4729927f020603c1992570e81fec7b9a0b7d2
Kate Buxton <k8hunter@hotmail.com>
2019-05-14 11:16:00 +0100

    image for film

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,29 @@ import "bootstrap";
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 initUpdateNavbarOnScroll();
+
+  const video = document.getElementById("video_home");
+  const volumeOn = document.getElementById("volume_on");
+  const volumeOff = document.getElementById("volume_off");
+  const vid = document.getElementById("video_home");
+
+  const width = window.screen.width
+  const src = document.getElementById("video-source").getAttribute('data-src')
+  if (width > 900) {
+    vid.setAttribute('src', src)
+  }
+  volumeOff.addEventListener('click', function() {
+    volumeOn.classList.toggle("hidden");
+    volumeOff.classList.toggle("hidden");
+    video.muted = false;
+  });
+
+  volumeOn.addEventListener('click', function() {
+    volumeOn.classList.toggle("hidden");
+    volumeOff.classList.toggle("hidden");
+    video.muted = true;
+  });
+
+  vid.onloadeddata = function() {
+    vid.style.opacity = 1
+  };
