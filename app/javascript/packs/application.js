b3363ef710c4bfc4ef29db063ddeff0f127a2302
Kate Buxton <k8hunter@hotmail.com>
2019-05-21 13:36:53 +0100

    video for show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,28 +3,16 @@ import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 initUpdateNavbarOnScroll();
 
-  const video = document.getElementById("video_home");
-  const volumeOn = document.getElementById("volume_on");
-  const volumeOff = document.getElementById("volume_off");
-  const vid = document.getElementById("video_home");
+const video = document.getElementById("video_home");
+const vid = document.getElementById("video_home");
 
-  const width = window.screen.width
-  const src = document.getElementById("video-source").getAttribute('data-src')
-  if (width > 900) {
-    vid.setAttribute('src', src)
-  }
-  volumeOff.addEventListener('click', function() {
-    volumeOn.classList.toggle("hidden");
-    volumeOff.classList.toggle("hidden");
-    video.muted = false;
-  });
+const width = window.screen.width
+const src = document.getElementById("video-source").getAttribute('data-src')
+{
+  vid.setAttribute('src', src)
+}
 
-  volumeOn.addEventListener('click', function() {
-    volumeOn.classList.toggle("hidden");
-    volumeOff.classList.toggle("hidden");
-    video.muted = true;
-  });
+vid.onloadeddata = function() {
+  vid.style.opacity = 1
+};
 
-  vid.onloadeddata = function() {
-    vid.style.opacity = 1
-  };
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources "contacts", only: [:new, :create]
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
+  get 'operation_surf', to: 'pages#operation_surf', as: :operation_surf
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
