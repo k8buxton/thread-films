7085b4a461b646ec86867c57d54ef9e714aeb17a
Kate Buxton <k8hunter@hotmail.com>
2019-05-23 12:31:43 +0100

    styling for about us

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
   get 'operation_surf', to: 'pages#operation_surf', as: :operation_surf
+  get 'aig', to: 'pages#aig', as: :aig
+  get 'forces', to: 'pages#forces', as: :forces
+  get 'football', to: 'pages#football', as: :football
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
