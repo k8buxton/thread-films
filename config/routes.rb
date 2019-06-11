f92ea3b3403ab2d9e6d4a9b2ceb4a7dd94fbd6e7
Kate Buxton <k8hunter@hotmail.com>
2019-06-11 05:58:31 +0100

    changed slug names

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   resources "contacts", only: [:new, :create]
   root to: 'pages#home'
   get 'about', to: 'pages#about', as: :about
-  get 'operation_surf', to: 'pages#operation_surf', as: :operation_surf
+  get 'endeavour', to: 'pages#endeavour', as: :endeavour
   get 'aig', to: 'pages#aig', as: :aig
-  get 'forces', to: 'pages#forces', as: :forces
+  get 'sugar', to: 'pages#sugar', as: :sugar
   get 'football', to: 'pages#football', as: :football
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
